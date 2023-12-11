8c90862d9c407d07b1eda56e71b14b54e5daf6be
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2023-12-11 01:36:02 +0100

    Update console.py

--- console.py
+++ console.py
@@ -1,6 +1,29 @@
 #!/usr/bin/python3
-"""Defines the HBnB console."""
-
+"""Defines the HBnB console, that manages the AirBnB objects.
+"""
 
 import cmd
-import re
+import re #regex
+from shlex import split #for parsing strings
+
+
+class HBNBCommand(cmd.Cmd):
+    """ Implements the hbnb command interpreter
+    Attributes:
+        prompt: The command prompt
+    """
+
+    prompt = "(hbnb) "
+
+    def emptyline(self):
+        """do nothing when an empty line is passed"""
+        pass
+
+    def do_EOF(self, argv):
+        """Exits the program upon receiving and EOF"""
+        print("")
+        return True
+
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()

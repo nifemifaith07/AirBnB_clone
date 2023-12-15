efdfceb621275f35dc840071fe6bfb3bc2018f17
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2023-12-15 21:27:49 +0100

    Create file_storage.py

---
+++ models/engine/file_storage.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines the file_storage class (an abstract storage engine)"""
+import json
+from models.base_model 
+import BaseModel
+"""from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Pla
+ce
+from models.amenity import Amenity
+from models.review import Review"""
+
+
+
+class FileStorage:
+    """Serializes instances to JSON file and deserializes JSON file to instances:
+    Attributes:
+        __file_path (str): The name of the file to save objects to.
+        __objects (dict): A dictionary of instantiated objects.
+    """
+    

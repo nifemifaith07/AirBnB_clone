e0891b11c3d42db0dbfdc85d256ff221161aa477
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2023-12-16 18:02:52 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 """Defines the file_storage class (an abstract storage engine)"""
 import json
-from models.base_model 
-import BaseModel
+from models.base_model import BaseModel
 """from models.user import User
 from models.state import State
 from models.city import City
-from models.place import Pla
-ce
+from models.place import Place
 from models.amenity import Amenity
 from models.review import Review"""
-
 
 
 class FileStorage:

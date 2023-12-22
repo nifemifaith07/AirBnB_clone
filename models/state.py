1afa816f6c4b4034f72d8f104ee8782723ee1ef8
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2023-12-22 13:30:32 +0100

    Create state.py

---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""defines the State class"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """represent the state
+    Attributes:
+        name (str): empty string
+
+    """
+    name = ""

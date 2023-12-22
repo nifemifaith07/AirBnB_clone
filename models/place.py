3be94740368675fc8a96ee20f43fd27c5b1bc69c
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2023-12-22 14:20:28 +0100

    Create place.py

---
+++ models/place.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Defines the Place class"""
+from models.base_model import BaseModel
+
+
+class Place(BaseModel):
+    """Represent the Place
+    Attributes:
+        city_id (str): empty string: it will be the City.id
+        user_id (str): empty string: it will be the User.id
+        name (str): empty string
+        description (str): empty string
+        number_rooms (int): 0
+        number_bathrooms (int): 0
+        max_guest (int): 0
+        price_by_night (int): 0
+        latitude (float): 0.0
+        longitude (float): 0.0
+        amenity_ids [str]: empty list: it will be the list of Amenity.id later
+    """
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
